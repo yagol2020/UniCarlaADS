0db5f4cafafe5f5d872e6324d0dbf8f3837e4c13
yagol2020 <yhw_yagol@nuaa.edu.cn>
2026-09-09 15:23:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# UniCarlaADS
+
+一个基于Docker的统一自动驾驶系统CARLA仿真运行框架
+
+# 目前支持的自动驾驶系统
+
+* InterFuser
+* LEAD
---
+++ service.py
@@ -0,0 +1,22 @@
+# UniCarlaADS的服务入口
+
+
+class ADS:
+    def __init__(self, name, port):
+        self.name = name
+        self.port = port
+        #
+        self.init()
+
+    def init(self):
+        pass
+
+    def deploy(self):
+        pass
+
+    def close(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
